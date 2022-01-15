f9c11f8250f489b051a018d84e31ee6bf3379555
Sanjay Kapoor <sanjay@jarna.com>
2022-01-15 17:57:42 -0600

    updated falcon config

--- falcon.rb
+++ falcon.rb
@@ -4,8 +4,10 @@
 load :rack, :supervisor
 
 hostname = File.basename(__dir__)
+
 rack hostname do
 	cache true
+  config_path {::File.expand_path("config.api.ru", root)}
   endpoint(Async::HTTP::Endpoint.parse("http://127.0.0.1:3001"))
 end
 
